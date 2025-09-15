16119ff853e0564868a8fcbafc9afc763d82fbe9
Carlos Magno <carmaso_@hotmail.com>
2025-09-15 17:39:45 -0300

    Main Prompt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import prompt from "prompt"
+import mainPrompt from "./prompts/prompt-main.js"
 
 async function main(){
+    prompt.get(mainPrompt, async (err, choose) => {
+        if(choose.select == 1) console.log("Escolheu o QRCODE");
+        if(choose.select == 2) console.log("Escolheu o PASSWORD");
+    });
+    prompt.start();
     
 }
-
-
-
 main();
